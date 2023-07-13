5fd49b6e7ce414f9a9826e81de7d463a5ac3faed
SushmaChandrasekar <130787806+SushmaChandrasekar@users.noreply.github.com>
2023-07-13 14:07:55 +0100

    Update GBCS_RandomForest.R

--- GBCS_RandomForest.R
+++ GBCS_RandomForest.R
@@ -1,7 +1,12 @@
+##############################################################################
+#-------------------------Random Forest model -------------------------------#
+##############################################################################
+
 # Load the required packages
 library(randomForest)
 library(caret)
 library(pROC)
+library(PRROC)
 
 # Load the dataset
 load("D:/SUSHMA DISSERTATION/GBCS.rdata")
@@ -66,3 +71,100 @@ cat("Recall:", recall, "\n")
 cat("Precision:", precision, "\n")
 cat("F1 Score:", f1_score, "\n")
 
+# Extract variable importance from the model
+var_imp <- varImp(rf_model, scale = FALSE)
+
+# Print the variable importance
+print(var_imp)
+
+# Plot the variable importance
+plot(var_imp)
+
+
+##############################################################################
+#----------------Logistic Regression with Elastic Net penalty----------------#
+##############################################################################
+
+
+
+# Load the required package
+library(glmnet)
+
+# Load the dataset
+load("D:/SUSHMA DISSERTATION/GBCS.rdata")
+ls()
+gbcs_data <- na.omit(gbcs)
+
+# Create a new dataset without transformations
+data <- data.frame(
+  age = gbcs_data$age,
+  size = gbcs_data$size,
+  nodes = gbcs_data$nodes,
+  prog_recp = gbcs_data$prog_recp,
+  estrg_recp = gbcs_data$estrg_recp,
+  hormone = as.factor(gbcs_data$hormone),
+  grade = as.factor(gbcs_data$grade),
+  censrec = as.factor(gbcs_data$censrec)
+)
+
+# Set the seed for reproducibility
+set.seed(6051)
+
+# Split the data into training and testing sets
+train_indices <- createDataPartition(data$censrec, p = 0.8, list = FALSE)
+train_data <- data[train_indices, ]
+test_data <- data[-train_indices, ]
+
+# Convert the response variable to numeric
+Y_train <- as.numeric(train_data$censrec) - 1
+Y_test <- as.numeric(test_data$censrec) - 1
+
+# Convert categorical variables into dummy variables (binary 0 and 1)
+# "-1" is to exclude the intercept term
+X_train <- model.matrix(~.-1, data = train_data[,-8])
+X_test <- model.matrix(~.-1, data = test_data[,-8])
+
+# Train the logistic regression model with elastic net penalty
+logistic_model <- cv.glmnet(X_train, Y_train, family = "binomial", alpha = 0.5, type.measure = "auc", nfolds = 10, standardize = TRUE)
+
+# Perform predictions on the test data
+test_predictions <- predict(logistic_model, newx = X_test, s = logistic_model$lambda.min, type = "response")
+
+
+# Calculate the AUC on the test data
+test_auc <- roc(as.vector(Y_test), as.vector(test_predictions))$auc
+
+# Print the results
+cat("Test AUC:", test_auc, "\n")
+
+# Plot the ROC curve
+roc_obj <- roc(as.vector(Y_test), as.vector(test_predictions))
+plot(roc_obj, main = "Receiver Operating Characteristic (ROC) Curve", print.auc = TRUE)
+
+# Plot AUC for each lambda value
+plot(logistic_model$lambda, logistic_model$cvm, type = "p", log = "x",
+     xlab = "Log(Lambda)", ylab = "Cross-Validated AUC",
+     main = "Cross-Validated AUC for Different Lambda Values")
+
+
+# Create a data frame with lambda and AUC values
+df <- data.frame(lambda = logistic_model$lambda, AUC = logistic_model$cvm)
+
+# Find the row with the maximum AUC
+max_row <- which.max(df$AUC)
+
+# Find the maximum AUC
+max_AUC <- df$AUC[max_row]
+
+# Create the base plot
+plot(df$lambda, df$AUC, type = "p", log = "x", xlab = "Log(Lambda)", 
+     ylab = "Cross-Validated AUC", main = "Cross-Validated AUC for Different Lambda Values",
+     col = ifelse(1:nrow(df) == max_row, "red", "black"), # Change the color of the maximum AUC point to red
+     pch = ifelse(1:nrow(df) == max_row, 19, 1), # Change the shape of the maximum AUC point to a solid circle
+     cex = ifelse(1:nrow(df) == max_row, 2, 1)) # Change the size of the maximum AUC point to be larger
+
+# Add a text to display the maximum AUC
+text(df$lambda[max_row], df$AUC[max_row]- 0.005, labels = paste("Max AUC =", round(max_AUC, 3)), pos = 4, cex = 0.8)
+
+# Add a legend
+legend("bottomright", legend = c("Maximum AUC", "Other values"), pch = c(19, 1), col = c("red", "black"))
